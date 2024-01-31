a14370919fdf6059abe95720aec9c902fe3f32ea
Hans Bergren (0xCLARITY) <hansbergren@gmail.com>
2024-01-30 18:33:18 -0800

    Add API endpoint

---
+++ app/api/frame/route.ts
@@ -0,0 +1,34 @@
+import {
+  FrameRequest,
+  getFrameAccountAddress,
+  getFrameMessage,
+} from "@coinbase/onchainkit";
+import { NextRequest, NextResponse } from "next/server";
+
+async function getResponse(req: NextRequest): Promise<NextResponse> {
+  let accountAddress: string | undefined = "";
+  const body: FrameRequest = await req.json();
+  const { isValid, message } = await getFrameMessage(body);
+
+  if (isValid) {
+    try {
+      accountAddress = await getFrameAccountAddress(message, {
+        NEYNAR_API_KEY: process.env.NEYNAR_API_KEY || "",
+      });
+    } catch (err) {
+      console.error(err);
+    }
+  }
+
+  return new NextResponse(`<!DOCTYPE html><html><head>
+    <meta property="fc:frame" content="vNext" />
+    <meta property="fc:frame:button:1" content="${accountAddress}" />
+    <meta property="fc:frame:post_url" content="https://localhost:3000/api/frame" />
+  </head></html>`);
+}
+
+export async function POST(req: NextRequest): Promise<Response> {
+  return getResponse(req);
+}
+
+export const dynamic = "force-dynamic";
