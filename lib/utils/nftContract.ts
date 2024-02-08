d8fead9c868dc74d39d0094c3476ceb1f2746a87
Hans Bergren (0xCLARITY) <hansbergren@gmail.com>
2024-02-08 13:15:41 -0800

    ENV polish

--- lib/utils/nftContract.ts
+++ lib/utils/nftContract.ts
@@ -8,35 +8,39 @@ import {
 import { baseSepolia, base } from 'viem/chains';
 import { privateKeyToAccount } from 'viem/accounts';
 
-// TODO: Make this dependent on an ENV VAR
-// - chainId
-const publicClient = createPublicClient({
-  chain: base,
-  transport: http(process.env.MAINNET_ALCHEMY_URL),
-});
+// Helper to get the viem chain and RPC_URL based on `process.env.CHAIN_ID`
+function getChain(): [typeof baseSepolia | typeof base, string | undefined] {
+  switch (process.env.CHAIN_ID) {
+    case baseSepolia.id.toString():
+      return [baseSepolia, process.env.BASE_SEPOLIA_ALCHEMY_URL];
 
+    case base.id.toString():
+      return [base, process.env.BASE_MAINNET_ALCHEMY_URL];
+
+    default:
+      throw new Error('Invalid chain id');
+  }
+}
+
+// Helper to get a contract instance with ability to read/write from the contract
 export function getContract<T extends Abi>(
   address: `0x${string}`,
   abi: T,
   privateKey: `0x${string}`,
 ) {
-  // TODO: Make privateKey an optional field
-  // If we don't have a private key, we can only use the public client
-  // if (!privateKey) {
-  //   return viemGetContract({
-  //     address,
-  //     abi,
-  //     client: publicClient,
-  //   });
-  // }
+  const [chain, rpcUrl] = getChain();
+
+  const publicClient = createPublicClient({
+    chain,
+    transport: http(rpcUrl),
+  });
 
   // If we have a private key, we can use the wallet client
   const account = privateKeyToAccount(privateKey);
   const walletClient = createWalletClient({
     account,
-    // TODO: Make this dependent on an ENV VAR
-    chain: base,
-    transport: http(process.env.MAINNET_ALCHEMY_URL),
+    chain,
+    transport: http(rpcUrl),
   });
 
   return viemGetContract({
